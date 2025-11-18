d410d3a9d313d04fdd29e84c88e6435e9e3bc5c4
joaoalbertorsc <jaoalbertorsc@gmail.com>
2025-11-18 16:47:47 -0300

    feat: Update DATABASE_URL in config to point to the new god-level-challenge database

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://challenge:challenge_2024@postgres:5432/challenge_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://challenge:challenge_2024@postgres:5432/god-level-challenge")
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
